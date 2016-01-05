1d21a75188f9342d05e23cb0bad9a5488789892d
Daphne <dg1181@nyu.edu>
2016-01-04 18:27:10 -0800

    Finished day 1

--- day1.py
+++ day1.py
@@ -1,6 +1,14 @@
-with open('day1.txt', 'r') as puzzle:
-    puzzletext = puzzle.read()
+def find_floor(puzzletext):
     up = puzzletext.count('(')
     down = puzzletext.count(')')
+    return up - down
 
-print(up-down)
+with open('day1.txt', 'r') as puzzle:
+    puzzletext = puzzle.read()
+
+print(find_floor(puzzletext))
+
+for i in range(len(puzzletext)):
+     if find_floor(puzzletext[0:i+1]) == -1:
+         print(i+1)
+         break
--- day1.rb
+++ day1.rb
@@ -1,8 +1,26 @@
+## Part 1
+
 puzzle = File.read("./day1.txt")
 
-up = puzzle.count("(")
-down = puzzle.count(")")
+def find_floor(puzzle)
+  up = puzzle.count("(")
+  down = puzzle.count(")")
 
-floor = up - down
+  return up - down
+end
 
-puts floor
+puts "Santa is on floor #{find_floor(puzzle)}"
+
+## Part 2
+
+current_floor = 0
+
+puzzle.length.times do |i|
+  current_slice = puzzle.slice(0..i)
+  current_floor = find_floor(current_slice)
+
+  if current_floor == -1
+    puts "Santa enters the basement at index #{i + 1}"
+    break
+  end
+end
